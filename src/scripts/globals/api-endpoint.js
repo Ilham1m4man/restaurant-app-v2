1e44425a9dc828672840e36ef133858f61e8a5dd
Ilham1m4man <kucingdiki@gmail.com>
2022-11-08 23:05:23 +0700

    Add picture size endpoint

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -2,8 +2,9 @@ import CONFIG from './config';
 
 const API_ENDPOINT = {
   RESTAURANT_LIST: `${CONFIG.BASE_URL}list`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}detail/:${id}`,
-  PICTURE: (pictureId) => `${CONFIG.BASE_URL}images/medium/${pictureId}`,
+  DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
+  PICTURE_MEDIUM: (pictureId) => `${CONFIG.BASE_URL}images/medium/${pictureId}`,
+  PICTURE_LARGE: (pictureId) => `${CONFIG.BASE_URL}images/large/${pictureId}`,
 };
 
 export default API_ENDPOINT;
