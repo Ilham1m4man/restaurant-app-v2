0607f81d1bb80c7e428199d0112a921036213e3a
Ilham1m4man <kucingdiki@gmail.com>
2022-11-08 23:06:18 +0700

    Still working on styling

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,17 +1,19 @@
 import UrlParser from "../../routes/url-parser";
 import RestaurantAppDbSource from "../../data/restaurantappdb-source";
+import { createRestaurantDetailTemplate } from "../templates/template-creator";
 
 const Detail = {
   async render() {
     return `
-      <h2>Detail Page</h2>
+      <div id="restaurant" class="restaurant"></div>
     `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
-    const movie = await RestaurantAppDbSource.detailRestaurant(url.id);
-    console.log(movie);
+    const restaurant = await RestaurantAppDbSource.detailRestaurant(url.id);
+    const restaurantsContainer = document.querySelector('#restaurant');
+    restaurantsContainer.innerHTML = createRestaurantDetailTemplate(restaurant.restaurant);
   },
 };
 
