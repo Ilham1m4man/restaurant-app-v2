5580200af9cbb1fe52c8e55e0047aa8f9c5cd273
Ilham1m4man <kucingdiki@gmail.com>
2022-11-08 23:07:42 +0700

    Deleting commented code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,24 +4,6 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 
-/* const menu = document.querySelector('#header__menu');
-const hero = document.querySelector('.hero');
-const main = document.querySelector('main');
-const navList = document.querySelector('.list-container');
-
-['click', 'keydown'].map((event) => menu.addEventListener(event, (e) => {
-  navList.classList.toggle('open');
-  e.stopPropagation();
-}));
-
-['click', 'keydown'].map((event) => hero.addEventListener(event, () => {
-  navList.classList.remove('open');
-}));
-
-['click', 'keydown'].map((event) => main.addEventListener(event, () => {
-  navList.classList.remove('open');
-})); */
-
 const app = new App({
   button: document.querySelector('#header__menu'),
   drawer: document.querySelector('.list-container'),
@@ -35,5 +17,3 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
 });
-
-console.log('Hello Coders! :)');
