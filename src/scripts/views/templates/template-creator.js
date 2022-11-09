a0b8a31ca878cce71f296736827059fd8541783b
Ilham1m4man <kucingdiki@gmail.com>
2022-11-10 01:08:50 +0700

    Changing the format to look good

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -11,32 +11,47 @@ const createRestaurantDetailTemplate = (restaurant) => `
   </div>
   <div class="restaurant__info">
     <h3>Information</h3>
-    <h4>⭐ ${restaurant.rating}</h4>
-    <h4>Address</h4>
-    <p>${restaurant.address}, ${restaurant.city}</p>
-    <h4>Description</h4>
-    <p>${restaurant.description}</p>
-    <h4>Categories</h4>
-    <div class="categories-container">
-      ${restaurant.categories.map((category) => (`<span class="category">${category.name}</span>`))}
+    <h4 id="rating" class="info__section">⭐ ${restaurant.rating}</h4>
+    <div id="address" class="info__section">
+      <h4>Address</h4>
+      <p>${restaurant.address}, ${restaurant.city}</p>
     </div>
-    <h4>Menus</h4>
-    <div class="menus-container">
-      <div class="foods-container">
-        ${restaurant.menus.foods.map((food) => (`<span class="food">${food.name}</span>`))}
+    <div id="categories" class="info__section">
+      <h4>Categories</h4>
+      <div class="categories-container">
+        ${restaurant.categories.map((category) => (`<span class="category">${category.name}</span>`)).join("")}
       </div>
-      <div class="drinks-container">
-        ${restaurant.menus.drinks.map((drink) => (`<span class="drink">${drink.name}</span>`))}
+    </div>
+    <div id="description" class="info__section">
+      <h4>Description</h4>
+      <p>${restaurant.description}</p>
+    </div>
+    <div id="menus" class="info__section">
+      <h4>Menu</h4>
+      <div class="menus-container">
+        <div class="foods-container">
+          <h5>Foods</h5>
+          <ul>
+            ${restaurant.menus.foods.map((food) => (`<li>${food.name}</li>`)).join("")}
+          </ul>
+        </div>
+        <div class="drinks-container">
+          <h5>Drinks</h5>
+          <ul>
+            ${restaurant.menus.drinks.map((drink) => (`<li>${drink.name}</li>`)).join("")}
+          </ul>
+        </div>
       </div>
+    </div>
   </div>
   <div class="restaurant__reviews">
     <h3>Let's see what others think about ${restaurant.name}</h3>
     <ul class="reviews-container">
       ${restaurant.customerReviews.map((reviewer) => (
         `<li>
+          <div class="review-date"><p>${reviewer.date}</p></div>
           <div class="review-name"><h4>${reviewer.name}</h4></div>
           <div class="review-reviewer"><p>${reviewer.review}</p></div>
-          <div class="review-date"><p>${reviewer.date}</p></div>
         </li>`
         )).join("")}
     </ul>
