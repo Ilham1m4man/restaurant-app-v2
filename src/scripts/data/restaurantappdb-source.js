42bbcd6cf68531806d2c3dc41ae41d556f93f890
Ilham1m4man <kucingdiki@gmail.com>
2022-11-11 00:28:01 +0700

    deleting commented code

--- src/scripts/data/restaurantappdb-source.js
+++ src/scripts/data/restaurantappdb-source.js
@@ -7,18 +7,6 @@ class RestaurantAppDbSource {
     return responseJson.restaurants;
   }
 
-  /* static async pictureOfRestaurant(pictureId) {
-    const response = await fetch(API_ENDPOINT.PICTURE(pictureId));
-    const responseJson = await response.json();
-    return responseJson;
-  } */
-
-  /* static async upcomingMovies() {
-    const response = await fetch(API_ENDPOINT.UPCOMING);
-    const responseJson = await response.json();
-    return responseJson.results;
-  } */
-
   static async detailRestaurant(id) {
     const response = await fetch(API_ENDPOINT.DETAIL(id));
     return response.json();
