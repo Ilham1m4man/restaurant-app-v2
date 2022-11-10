2875a5f5732bb4aa4b2cc4dcb40e7eab487c06c6
Ilham1m4man <kucingdiki@gmail.com>
2022-11-11 00:30:27 +0700

    add favorite page file

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,16 +1,29 @@
-import RestaurantAppDbSource from "../../data/restaurantappdb-source";
+import FavoriteRestaurantIdb from "../../data/favorite-restaurant-idb";
+import { createRestaurantItemTemplate } from '../templates/template-creator';
 
 const Favorite = {
   async render() {
     return `
-      <h2>Favorite Page</h2>
+    <div class="content">
+      <div id="restaurants" class="restaurants-favorite posts">
+
+      </div>
+    </div>
     `;
   },
 
   async afterRender() {
-    const restaurants = await RestaurantAppDbSource.listOfRestaurant();
-    console.log(restaurants);
-    // Fungsi ini akan dipanggil setelah render()
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+    const restaurantsContainer = document.querySelector('#restaurants');
+
+    if (restaurants.length === 0) {
+      restaurantsContainer.innerHTML = `<h2 class="no-favorite">You Don't Have Any Favorite Restaurant Yet...</h2>`;
+      restaurantsContainer.classList.remove('posts');
+    } else {
+      restaurants.forEach((restaurant) => {
+        restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
+      });
+    }
   },
 };
 
