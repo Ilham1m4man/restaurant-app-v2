2c36a23dcdb363ec7327f78ea1cc3e0d6b7ea429
Ilham1m4man <kucingdiki@gmail.com>
2022-11-11 00:30:09 +0700

    Edit to add like btn initiator

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,19 +1,34 @@
 import UrlParser from "../../routes/url-parser";
 import RestaurantAppDbSource from "../../data/restaurantappdb-source";
 import { createRestaurantDetailTemplate } from "../templates/template-creator";
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 
 const Detail = {
   async render() {
     return `
-      <div id="restaurant" class="restaurant"></div>
+      <div id="restaurants" class="restaurant"></div>
+      <div id="likeButtonContainer"></div>
     `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await RestaurantAppDbSource.detailRestaurant(url.id);
-    const restaurantsContainer = document.querySelector('#restaurant');
-    restaurantsContainer.innerHTML = createRestaurantDetailTemplate(restaurant.restaurant);
+    const resto = restaurant.restaurant;
+    const restaurantsContainer = document.querySelector('#restaurants');
+    restaurantsContainer.innerHTML = createRestaurantDetailTemplate(resto);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: resto.id,
+        name: resto.name,
+        city: resto.city,
+        pictureId: resto.pictureId,
+        rating: resto.rating,
+        description: resto.description,
+      },
+    });
   },
 };
 
