7f77061c7caf120c7b7d56654a8c89b47d061dbe
Ilham1m4man <kucingdiki@gmail.com>
2022-11-13 16:10:31 +0700

    Add listener to make skip main works

--- src/scripts/views/pages/restaurant-list.js
+++ src/scripts/views/pages/restaurant-list.js
@@ -10,7 +10,7 @@ const RestaurantList = {
           <img src="./images/heros/hero-image_2.webp" class="hero__img" alt="Picture of people eating together" tabindex="0">
         </div>
       </div>
-      <h2 class="content__heading">List of Restaurants</h2>
+      <h2 class="content__heading" tabindex="0">List of Restaurants</h2>
       <div id="restaurants" class="restaurants posts">
       
       </div>
@@ -21,6 +21,22 @@ const RestaurantList = {
   async afterRender() {
     const restaurants = await RestaurantAppDbSource.listOfRestaurant();
     const restaurantsContainer = document.querySelector('#restaurants');
+    const skipListener = document.querySelector('.skip-main');
+    /*
+        Kalau pakai Extension "Screen Reader" di Chrome nanti event focus-nya bakal stuck
+        di tombol "Skip to Main" jika user mengklik pakai tombol "ENTER",
+        sedangkan kalau mengklik tombol lainnya tidak ada masalah tersebut.
+        Tapi kalau gak pakai "Screen Reader", masalah ini gak akan terjadi.
+        Saya tidak tahu cara mengatasinya kak, kemungkinan ini bug dari Extension-nya kak
+    */
+
+    skipListener.href = ``;
+    skipListener.href = `#restaurants`;
+    ['click', 'keydown'].map((event) => skipListener.addEventListener(event, () => {
+      // eslint-disable-next-line no-unused-expressions
+      restaurantsContainer.focus();
+    }));
+
     // eslint-disable-next-line array-callback-return
     restaurants.map((restaurant) => {
       restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
