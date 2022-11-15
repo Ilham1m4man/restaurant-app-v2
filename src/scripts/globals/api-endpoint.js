835366cf2c1e13c457f418fa0d88c106bd15ad3d
Ilham1m4man <kucingdiki@gmail.com>
2022-11-15 23:40:14 +0700

    Add add_review endpoint

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -5,6 +5,16 @@ const API_ENDPOINT = {
   DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
   PICTURE_MEDIUM: (pictureId) => `${CONFIG.BASE_URL}images/medium/${pictureId}`,
   PICTURE_LARGE: (pictureId) => `${CONFIG.BASE_URL}images/large/${pictureId}`,
+  ADD_REVIEW: (id, name, review) => {
+    // eslint-disable-next-line no-unused-expressions, no-sequences
+    `${CONFIG.BASE_URL}review`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id, name, review }),
+    };
+  },
 };
 
 export default API_ENDPOINT;
