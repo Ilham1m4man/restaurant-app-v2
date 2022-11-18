115f6a1c5c072e0ac44eb8e1afc19274298a51a3
Ilham1m4man <kucingdiki@gmail.com>
2022-11-19 01:02:37 +0700

    Trying to use Search&Show presenter Searchview too

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,28 +1,23 @@
+/* eslint-disable no-new */
 import FavoriteRestaurantIdb from "../../data/favorite-restaurant-idb";
 import { createRestaurantItemTemplate } from '../templates/template-creator';
+import FavoriteRestaurantSearchView from "./liked-restaurants/favorite-restaurant-search-view";
+import FavoriteRestaurantSearchPresenter from "./liked-restaurants/favorite-restaurant-search-presenter";
+import FavoriteRestaurantShowPresenter from "./liked-restaurants/favorite-restaurant-show-presenter";
+
+const view = new FavoriteRestaurantSearchView();
 
 const Favorite = {
   async render() {
-    return `
-    <div class="content">
-      <div id="restaurants-favorite" class="restaurants-favorite posts" tabindex="0" aria-label="List of your favorite restaurants">
-
-      </div>
-    </div>
-    `;
+    return view.getTemplate();
   },
 
   async afterRender() {
+    /* new FavoriteRestaurantShowPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb });
+    new FavoriteRestaurantSearchPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb }); */
     const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
-    const restaurantsContainer = document.querySelector('#restaurants-favorite');
+    const restaurantsContainer = document.querySelector('#restaurants');
     const skipListener = document.querySelector('.skip-main');
-    /*
-        Kalau pakai Extension "Screen Reader" di Chrome nanti event focus-nya bakal stuck
-        di tombol "Skip to Main" jika user mengklik pakai tombol "ENTER",
-        sedangkan kalau mengklik tombol lainnya tidak ada masalah tersebut.
-        Tapi kalau gak pakai "Screen Reader", masalah ini gak akan terjadi.
-        Saya tidak tahu cara mengatasinya kak, kemungkinan ini bug dari Extension-nya kak
-    */
 
     skipListener.href = ``;
     skipListener.href = `/#/favorite`;
@@ -39,6 +34,8 @@ const Favorite = {
         restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
       });
     }
+    const loaderContainer = document.querySelector('#loader');
+    loaderContainer.style.display = 'none';
   },
 };
 
