a40d1cc4bf780071f31f8701ca4666d3a8af64ae
Ilham1m4man <kucingdiki@gmail.com>
2022-11-19 01:08:24 +0700

    Add PWA manifest plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -37,6 +38,23 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new WebpackPwaManifest({
+      id: "restaurant-app-2.0",
+      start_url: "./index.html",
+      name: "Restaurant App 2.0",
+      short_name: "Restaurant App",
+      description: "Free App For Finding The Best Restaurant",
+      display: "standalone",
+      background_color: "#222831",
+      theme_color: "#DDDDDD",
+      icons: [
+        {
+          src: path.resolve('src/public/icons/icon-512x512.png'),
+          sizes: [72, 96, 128, 144, 152, 192, 384, 512],
+          purpose: 'maskable',
         },
       ],
     }),
