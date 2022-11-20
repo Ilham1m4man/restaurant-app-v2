049f64ab795bfb773093fd7a8cf998336a9e2e52
Ilham1m4man <kucingdiki@gmail.com>
2022-11-20 19:53:37 +0700

    Add search presenter file

---
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-presenter.js
@@ -0,0 +1,36 @@
+class FavoriteRestaurantSearchPresenter {
+  constructor({ view, favoriteRestaurants }) {
+    this._view = view;
+    this._listenToSearchRequestByUser();
+    this._favoriteRestaurants = favoriteRestaurants;
+  }
+
+  _listenToSearchRequestByUser() {
+    this._view.runWhenUserIsSearching((latestQuery) => {
+      this._searchRestaurants(latestQuery);
+    });
+  }
+
+  async _searchRestaurants(latestQuery) {
+    this._latestQuery = latestQuery.trim();
+
+    let foundRestaurants;
+    if (this.latestQuery.length > 0) {
+      foundRestaurants = await this._favoriteRestaurants.searchRestaurants(this._latestQuery);
+    } else {
+      foundRestaurants = await this._favoriteRestaurants.getAllRestaurants();
+    }
+
+    this._showRestaurants(foundRestaurants);
+  }
+
+  _showRestaurants(restaurants) {
+    this._view.showFavoriteRestaurants(restaurants);
+  }
+
+  get latestQuery() {
+    return this._latestQuery;
+  }
+}
+
+export default FavoriteRestaurantSearchPresenter;
