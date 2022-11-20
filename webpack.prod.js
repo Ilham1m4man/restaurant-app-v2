513b6e7a158961a5e5ba70f662c2ebc4926ed168
Ilham1m4man <kucingdiki@gmail.com>
2022-11-20 19:50:22 +0700

    Add imagemin plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const imageminWebp = require('imagemin-webp');
+const imageminPngquant = require('imagemin-pngquant');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -23,9 +26,22 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
+    new ImageminWebpackPlugin({
+      plugins: [
+        imageminWebp({
+          quality: 50,
+        }),
+        imageminPngquant({
+          quality: [0.5, 0.5],
+        }),
+      ],
+    }),
     new WorkboxWebpackPlugin.InjectManifest({
       swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
       swDest: './sw.bundle.js',
     }),
   ],
+  externals: {
+    sharp: 'sharp.js',
+  },
 });
