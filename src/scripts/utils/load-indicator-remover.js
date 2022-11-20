9c5489aa7f3e6fdb8aefd1878d9d7f4be2a7aadc
Ilham1m4man <kucingdiki@gmail.com>
2022-11-20 19:53:07 +0700

    Add file load indicator remover file

---
+++ src/scripts/utils/load-indicator-remover.js
@@ -0,0 +1,6 @@
+const loadIndicatorRemover = () => {
+  const loaderContainer = document.querySelector('#loader');
+  loaderContainer.style.display = 'none';
+};
+
+export default loadIndicatorRemover;
