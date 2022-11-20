437d5c46b834cad6b61316ac4e9d00bc13dd3b81
Ilham1m4man <kucingdiki@gmail.com>
2022-11-20 19:52:16 +0700

    Add PICTURE_SMALL & removed SEARCH endpoint

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -3,10 +3,10 @@ import CONFIG from './config';
 const API_ENDPOINT = {
   RESTAURANT_LIST: `${CONFIG.BASE_URL}list`,
   DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
+  PICTURE_SMALL: (pictureId) => `${CONFIG.BASE_URL}images/small/${pictureId}`,
   PICTURE_MEDIUM: (pictureId) => `${CONFIG.BASE_URL}images/medium/${pictureId}`,
   PICTURE_LARGE: (pictureId) => `${CONFIG.BASE_URL}images/large/${pictureId}`,
   ADD_REVIEW: `${CONFIG.BASE_URL}review`,
-  SEARCH: (query) => `${CONFIG.BASE_URL}/search?q=${query}`,
 };
 
 export default API_ENDPOINT;
